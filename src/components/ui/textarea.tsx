6a96126f42be6644e32598589a12379dc38e00df
Ivan B <sensenabi@gmail.com>
2023-09-05 15:21:18 -0500

    fix lint

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 
 import { cn } from "~/lib/utils"
 
+// eslint-disable-next-line
 export interface TextareaProps
   extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
 
