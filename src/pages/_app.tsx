3b535fd8c239f9ab9763735a60b58ea984ef3740
Ivan B <sensenabi@gmail.com>
2023-09-05 15:11:04 -0500

    add rich colors

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,15 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { Toaster } from "sonner";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />;
+      <Toaster richColors />
+    </>
+  );
 };
 
 export default api.withTRPC(MyApp);
