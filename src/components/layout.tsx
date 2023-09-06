dfb5863b8d8a5289ab6442cc74dbb39a42d5ea2e
Ivan B <sensenabi@gmail.com>
2023-09-05 19:00:09 -0500

    wip: home

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,20 +5,25 @@ import Link from "next/link";
 
 function SideMenu() {
   return (
-    <Sheet>
-      <SheetTrigger asChild>
-        <Menu className="h-8 w-8" />
-      </SheetTrigger>
-      <SheetContent side={"left"}>
-        <h1 className="font-semibold">Nutrivision</h1>
-        <Link href={"/family"} className={buttonVariants({
-          variant: "ghost"
-        })}>
-        <Users2 className="h-5 w-5 mr-2" />
-        Grupo familiar
-      </Link>
-    </SheetContent>
-    </Sheet >
+    <div className="p-5">
+      <Sheet>
+        <SheetTrigger asChild>
+          <Menu className="h-6 w-6" />
+        </SheetTrigger>
+        <SheetContent side={"left"}>
+          <h1 className="font-semibold">Nutrivision</h1>
+          <Link
+            href={"/family"}
+            className={buttonVariants({
+              variant: "ghost",
+            })}
+          >
+            <Users2 className="mr-2 h-5 w-5" />
+            Grupo familiar
+          </Link>
+        </SheetContent>
+      </Sheet>
+    </div>
   );
 }
 
